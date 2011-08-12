72735b8e9068237a530a984d8874b215740bde9f
victornava <victornavav@gmail.com>
2011-08-13 03:28:41 +1000

    added whole zend lib

--- mailer.php
+++ mailer.php
@@ -1,8 +1,8 @@
 <?php
 
-$emails = array("victornavav@gmail.com", "eduardonavav@gmail.com", "emil@kjer.info", 'yannis.kolovos@gmail.com');
+require 'Zend/Mail.php';
 
-// require 'Zend/Mail.php';
+// $emails = array("victornavav@gmail.com");
 // 
 // $mail = new Zend_Mail();
 // $mail->setSubject("My Email with Attachment");
@@ -17,20 +17,27 @@ $emails = array("victornavav@gmail.com", "eduardonavav@gmail.com", "emil@kjer.in
 // $attachment->filename = 'achunk.pdf';
 // $mail->send();
 
-function send_mail($to, $body, $attachment){
-	
+function send_mail($emails, $subject, $body, $attachment){
 	$mail = new Zend_Mail();
-	$mail->setSubject("My Email with Attachment");
-
+	$mail->setSubject($subject);
+	
 	foreach($emails as $email){
 		$mail->addTo($email);
 	}
-
+	
 	$mail->setBodyText("Here the first chunk! get exited");
 	$attachment = $mail->createAttachment(file_get_contents('a.pdf'));
 	$attachment->type = 'application/pdf';
 	$attachment->filename = 'achunk.pdf';
 	$mail->send();
 }
+// $emails = array("victornavav@gmail.com", "eduardonavav@gmail.com", "emil@kjer.info", 'yannis.kolovos@gmail.com');
+$emails = array("victornavav@gmail.com");
+$subject = "read a chunk";
+$body = "Here the first chunk! get exited";
+$attachment = file_get_contents('a.pdf');
+
+echo send_mail($emails, $subject, $body, $attachment);
+echo "sent";
 
 ?>
