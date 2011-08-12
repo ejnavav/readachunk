8fb491b338f8bc3ad5daae1b8ad12175a4367f53
victornava <victornavav@gmail.com>
2011-08-13 03:35:30 +1000

    adding mail with attachment funtionality

--- mailer.php
+++ mailer.php
@@ -2,21 +2,6 @@
 
 require 'Zend/Mail.php';
 
-// $emails = array("victornavav@gmail.com");
-// 
-// $mail = new Zend_Mail();
-// $mail->setSubject("My Email with Attachment");
-// 
-// foreach($emails as $email){
-// 	$mail->addTo($email);
-// }
-// 
-// $mail->setBodyText("Here the first chunk! get exited");
-// $attachment = $mail->createAttachment(file_get_contents('a.pdf'));
-// $attachment->type = 'application/pdf';
-// $attachment->filename = 'achunk.pdf';
-// $mail->send();
-
 function send_mail($emails, $subject, $body, $attachment){
 	$mail = new Zend_Mail();
 	$mail->setSubject($subject);
@@ -24,20 +9,23 @@ function send_mail($emails, $subject, $body, $attachment){
 	foreach($emails as $email){
 		$mail->addTo($email);
 	}
-	
-	$mail->setBodyText("Here the first chunk! get exited");
+		
+	$mail->setBodyText($body);
 	$attachment = $mail->createAttachment(file_get_contents('a.pdf'));
 	$attachment->type = 'application/pdf';
 	$attachment->filename = 'achunk.pdf';
-	$mail->send();
+	return $mail->send();
 }
-// $emails = array("victornavav@gmail.com", "eduardonavav@gmail.com", "emil@kjer.info", 'yannis.kolovos@gmail.com');
-$emails = array("victornavav@gmail.com");
-$subject = "read a chunk";
-$body = "Here the first chunk! get exited";
-$attachment = file_get_contents('a.pdf');
 
-echo send_mail($emails, $subject, $body, $attachment);
-echo "sent";
+function test_send_mail(){
+	// $emails = array("victornavav@gmail.com", "eduardonavav@gmail.com", "emil@kjer.info", 'yannis.kolovos@gmail.com');
+	$emails = array("victornavav@gmail.com");
+	$subject = "read a chunk";
+	$body = "The the dam thing now!";
+	$attachment = file_get_contents('a.pdf');
+	send_mail($emails, $subject, $body, $attachment);
+	return true;
+}
 
+// echo test_send_mail();
 ?>
