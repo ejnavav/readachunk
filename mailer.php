6f88352de23311c51980680e044062f8be9b85c9
victornava <victornavav@gmail.com>
2011-08-13 03:03:23 +1000

    adding zend library

---
+++ mailer.php
@@ -0,0 +1,20 @@
+<?php
+
+$emails = array("victornavav@gmail.com", "eduardonavav@gmail.com", "emil@kjer.info", 'yannis.kolovos@gmail.com');
+
+require 'Zend/Mail.php';
+
+$mail = new Zend_Mail();
+$mail->setSubject("My Email with Attachment");
+
+foreach($emails as $email){
+	$mail->addTo($email);
+}
+
+$mail->setBodyText("Here the first chunk! get exited");
+$attachment = $mail->createAttachment(file_get_contents('a.pdf'));
+$attachment->type = 'application/pdf';
+$attachment->filename = 'achunk.pdf';
+$mail->send();
+
+?>
