b18d3f42d7be27bff2b8b2ed6bd734a2bb4bd608
victornava <victornavav@gmail.com>
2011-08-13 01:13:20 +1000

    first

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Read A Chunk</title>
+</head>
+<body>
+	<div id="container" class ="container">
+		<h1>Read a Chunk</h1>
+	</div>
+	<div>
+		<form action="upload.php" method="get" accept-charset="utf-8">
+			<label for="book">This is my book</label><br / >
+			<input type="text" name="book" id="book"><br / >
+			<label for="book">This is my email</label><br / >
+			<input type="text" name="email"><br / >
+			<input type="submit" value="Go!">
+		</form>
+	</div>
+</body>
