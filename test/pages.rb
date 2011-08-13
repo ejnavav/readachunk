10c34a1f403860c9040ae5c59cd137d398a8cddd
victornava <victornavav@gmail.com>
2011-08-14 00:37:02 +1000

    test/pages.rb

--- test/pages.rb
+++ test/pages.rb
@@ -1,13 +1,18 @@
+host = ARGV[0] ||= "localhost"
+email = ARGV[1] ||= "email=readachunk@kjer.info"
+
+puts "host: #{host}"
+puts "email: #{email}"
 
 `rm ../uploads/*.pdf`
 `rm ../temp/*.pdf`
 `rm ../db.json`
 `touch ../db.json`
 
-puts `curl -F "book=@20-pages.pdf;type=application/pdf" -F "email=vhnv2g@gmail.com" -F "pages=5" -F "frequency=1" http://localhost/upload.php`
+puts `curl -F "book=@20-pages.pdf;type=application/pdf" -F "email=#{email}" -F "pages=5" -F "frequency=1" http://#{host}/upload.php`
 
 4.times do |i|
   puts "scheduller: #{i}"
   puts `curl http://localhost/scheduler.php`
-  sleep(5)
+  sleep(1)
 end  
