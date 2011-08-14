081b4a4d919cbd1be159c9b053e10e2f4451ef97
ejnavav <eduardonavav@gmail.com>
2011-08-14 10:49:33 +1000

    fixed HOST

--- common.php
+++ common.php
@@ -6,7 +6,7 @@
 define ("UPLOADFOLDERPATH", "uploads/");
 define ("TEMP_PATH", "temp/");
 
-define("HOST",$_SERVER['SERVERNAME'];
+define("HOST",$_SERVER['SERVERNAME']);
 
 require_once('fileuploader.php');
 require_once('pdf_chunker.php');
